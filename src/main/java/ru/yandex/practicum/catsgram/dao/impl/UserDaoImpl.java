bc9280e62de47a617ebdb864b7d829d878f551ac
xseeeds <s86ko@yandex.ru>
2023-04-10 23:43:38 +0400

    dao

---
+++ src/main/java/ru/yandex/practicum/catsgram/dao/impl/UserDaoImpl.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.catsgram.dao.impl;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.catsgram.dao.UserDao;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.util.Optional;
+
+@Component
+public class UserDaoImpl implements UserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDaoImpl(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
+
+    @Override
+    public Optional<User> findUserById(String id) {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.catsgram.model;
+
+public class User {
+
+    private String id;
+
+    private String username;
+
+    private String nickname;
+
+    public User() {
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getId() {
+        return id;
+    };
+
+}
