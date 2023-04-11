0f42e99de69726326d261d9774ade3d81dada17b
xseeeds <s86ko@yandex.ru>
2023-04-11 11:42:51 +0400

    dao

--- src/main/java/ru/yandex/practicum/catsgram/dao/impl/UserDaoImpl.java
+++ src/main/java/ru/yandex/practicum/catsgram/dao/impl/UserDaoImpl.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum.catsgram.dao.impl;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.catsgram.dao.UserDao;
 import ru.yandex.practicum.catsgram.model.User;
@@ -9,15 +13,27 @@ import java.util.Optional;
 
 @Component
 public class UserDaoImpl implements UserDao {
+    private final Logger log = LoggerFactory.getLogger(UserDaoImpl.class);
 
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDaoImpl(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate=jdbcTemplate;
+    public UserDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Optional<User> findUserById(String id) {
-        return Optional.empty();
+        // выполняем запрос к базе данных.
+        SqlRowSet userRows = jdbcTemplate.queryForRowSet("select * from cat_user where id = ?", id);
+        if (userRows.next()) {
+            log.info("Найден пользователь: {} {}", userRows.getString("id"), userRows.getString("nickname"));
+            // вы заполните данные пользователя в следующем уроке
+            User user = new User();
+            user.setId(id);
+            return Optional.of(user);
+        } else {
+            log.info("Пользователь с идентификатором {} не найден.", id);
+            return Optional.empty();
+        }
     }
 }
