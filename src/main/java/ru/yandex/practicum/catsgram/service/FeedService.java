521688805def24100873ef4e77d7348d7f46ac86
Sergey Kolganov <s86ko@yandex.ru>
2023-09-03 13:17:29 +0400

    DAO Don't create table users in AutoSchema.sql

--- src/main/java/ru/yandex/practicum/catsgram/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/FeedService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.catsgram.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.catsgram.dao.FollowDao;
 import ru.yandex.practicum.catsgram.model.Post;
@@ -7,12 +8,9 @@ import ru.yandex.practicum.catsgram.model.Post;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class FeedService {
     private final FollowDao followDao;
-
-    public FeedService(FollowDao followDao) {
-        this.followDao = followDao;
-    }
 
     public List<Post> getFeedFor(String userId, int max) {
         return followDao.getFollowFeed(userId, max);
